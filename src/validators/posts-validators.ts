0ba95b328761ce69f35b60ebe45e09cd7ae6cceb
stacy <ada.davis27@gmail.com>
2023-04-24 13:26:08 +0300

    hw3 v6.0

--- src/validators/posts-validators.ts
+++ src/validators/posts-validators.ts
@@ -1,22 +1,13 @@
-import {body} from 'express-validator';
 import {blogsRepository} from '../repositories/blogs-repository';
+import {createFieldChain} from './utils';
 
-export const postTitleValidator = body('title')
-    .isString().withMessage('Value \'title\' should be type string')
-    .trim().notEmpty().withMessage('Value \'title\' is required and shouldn\'t be empty')
-    .isLength({max: 30}).withMessage('Value \'title\' should be with max length of 30')
+export const postTitleValidator = createFieldChain('title', 30)
 
-export const postShortDescriptionValidator = body('shortDescription')
-    .isString().withMessage('Value \'shortDescription\' should be type string')
-    .trim().notEmpty().withMessage('Value \'shortDescription\' is required and shouldn\'t be empty')
-    .isLength({max: 100}).withMessage('Value \'shortDescription\' should be with max length of 100')
+export const postShortDescriptionValidator = createFieldChain('shortDescription', 100)
 
-export const postContentValidator = body('content')
-    .isString().withMessage('Value \'content\' should be type string')
-    .trim().notEmpty().withMessage('Value \'content\' is required and shouldn\'t be empty')
-    .isLength({max: 1000}).withMessage('Value \'content\' should be with max length of 1000')
+export const postContentValidator = createFieldChain('content', 1000)
 
-export const postBlogIdValidator = body('blogId')
+export const postBlogIdValidator = createFieldChain('blogId')
     .custom(async blogId => {
         const blog = await blogsRepository.findBlogById(blogId)
 
@@ -24,5 +15,3 @@ export const postBlogIdValidator = body('blogId')
             throw new Error('Value \'blogId\' should be an id of existing blog');
         }
     })
-    .isString().withMessage('Value \'blogId\' should be type string')
-    .trim().notEmpty().withMessage('Value \'blogId\' is required and shouldn\'t be empty')
--- src/validators/videos-validators.ts
+++ src/validators/videos-validators.ts
@@ -1,5 +1,3 @@
-import {body} from 'express-validator';
+import {createFieldChain} from './utils';
 
-export const titleValidator = body('title').isString().withMessage('Value \'title\' should be type string')
-    .trim().notEmpty().withMessage('Value \'title\' is required and shouldn\'t be empty')
-    .isLength({max: 40}).withMessage('Value \'title\' should be with max length of 40')
+export const titleValidator = createFieldChain('title', 40)
