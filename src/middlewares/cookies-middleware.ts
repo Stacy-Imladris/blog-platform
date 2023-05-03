3291792912f88a3a1ae50dc17a3768283d30a19c
stacy <ada.davis27@gmail.com>
2023-05-04 00:13:27 +0300

    hw8 v3.0

--- src/middlewares/cookies-middleware.ts
+++ src/middlewares/cookies-middleware.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from 'express';
 import {HTTP_STATUSES} from '../utils';
 import {jwtService} from '../application/jwt-service';
+import {sessionsQueryRepository} from '../repositories/sessions-query-repository';
 
 export const cookiesMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
@@ -12,7 +13,9 @@ export const cookiesMiddleware = async (req: Request, res: Response, next: NextF
 
     const verifiedData = await jwtService.verifyUserByToken(refreshToken)
 
-    if (!verifiedData) {
+    const session = await sessionsQueryRepository.findSessionById(verifiedData?.sessionId!)
+
+    if (!verifiedData || !session || session.refreshToken !== refreshToken) {
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZED_401)
         return
     }
